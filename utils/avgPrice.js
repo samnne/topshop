ae60b052740c57a13b1dcedb970482c07ba27f37
Samuel Echefu <mashce@uvic.ca>
2025-12-12 02:36:40 -0800

    New api config and restritive schema, testing for production

--- utils/avgPrice.js
+++ utils/avgPrice.js
@@ -1,29 +1,42 @@
-const gemini = require("@google/genai");
+const { GoogleGenAI } = require("@google/genai");
 const Product = require("../models/product");
 const AppError = require("./AppError");
+
 const question = (theName) => {
   return `
   Whats the best range price of the average price and keep the lower and upper limit
   as close as possible for ${theName} and give just the number (can be float) 
-  do NOT put curly braces in between it eg. {1.99-2.99} and for the accurancy 
-  make sure its within a +/- $2-3 differnece for both low and high end range
+  eg. 1.99-2.99 and for the accurancy make sure its within a +/- $2-3 differnece for both low and high end range
   do NOT put the dollar sign in your response from general supermarkets 
   (Walmart, No Frills, Loblaws, etc.) (The price should be in CAD dollars) `;
 };
 async function avgPrice(product, next, req) {
-  const ai = new gemini.GoogleGenAI({ apiKey: process.env.API_KEY });
+  const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
+
   try {
     const response = await ai.models.generateContent({
-      model: "gemini-2.0-flash",
+      model: "gemini-2.5-flash-lite",
       contents: question(product.name),
+      config: {
+        responseMimeType: "application/json",
+        responseSchema: {
+          type: "string",
+        },
+      },
     });
-    const newPrice = response.text;
+    const price = response.text;
+    let newPrice = ""
+    for (let i = 0; i < price.length; i++){
+      if (price[i]===`"`) continue;
+      newPrice += price[i]
+    }
+  
     const updatedProduct = await Product.findByIdAndUpdate(product._id, {
       price: newPrice,
     });
-    console.log("worked")
+    console.log("worked");
   } catch (err) {
-    console.log("reached error")
+    console.log("reached error", err);
     req.flash("error", "Error when computing a price!".toUpperCase());
     next(new AppError(err.message, err.code));
   }
@@ -34,27 +47,26 @@ function roundToDecimal(number, decimals = 2) {
   return Math.round(number * factor) / factor;
 }
 
-function scanPrice(price){
-
-
-    let dash = '-';
-    let found = false;
-    let lowerLimit = "";
-    let upperLimit = "";
-    for(let i = 0; i < price.length; i++){
-        if(price[i] === dash){
-            found = true;
-            continue;
-        }
-        if(found){
-            upperLimit += price[i];
-        } else {
-            lowerLimit += price[i];
-        }
+function scanPrice(price) {
+  let dash = "-";
+  let found = false;
+  let lowerLimit = "";
+  let upperLimit = "";
+  for (let i = 0; i < price.length; i++) {
+    if (price[i] === `"`) continue;
+    if (price[i] === dash) {
+      found = true;
+      continue;
     }
-    lowerLimit = roundToDecimal(parseFloat(lowerLimit))
-    upperLimit = roundToDecimal(parseFloat(upperLimit))
-    return `${lowerLimit}-${upperLimit}`
+    if (found) {
+      upperLimit += price[i];
+    } else {
+      lowerLimit += price[i];
+    }
+  }
+  lowerLimit = roundToDecimal(parseFloat(lowerLimit));
+  upperLimit = roundToDecimal(parseFloat(upperLimit));
+  return `${lowerLimit}-${upperLimit}`;
 }
 
 function calcTotal(allProducts = []) {
@@ -62,10 +74,11 @@ function calcTotal(allProducts = []) {
   let lowerLimit = 0;
   let upperLimit = 0;
   allProducts.forEach((cur) => {
-    let dash = cur.price.indexOf('-')
+    let dash = cur.price.indexOf("-");
     if (cur.buy === "on") {
-      lowerLimit += parseFloat(cur.price.slice(0, dash)) * cur.qty;
-      upperLimit += parseFloat(cur.price.slice(dash + 1, cur.price.length)) * cur.qty;
+      lowerLimit += parseFloat(cur.price.slice(1, dash)) * cur.qty;
+      upperLimit +=
+        parseFloat(cur.price.slice(dash + 1, cur.price.length)) * cur.qty;
     }
     //console.log(`price: ${cur.price} lower upper: ${lowerLimit} ${upperLimit}`)
   });
